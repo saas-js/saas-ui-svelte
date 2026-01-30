c3cf3b806f07630ceb2b2feb8edd76435d1f442d
Jacob Weinbren <jacobweinbren@gmail.com>
2026-01-30 00:43:31 +0000

    Fixed docs navigation

---
+++ apps/website/src/components/docs/CodeBlockInner.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Box } from "@saas-ui/svelte/layout/box";
+	import { Clipboard } from "@saas-ui/svelte/components/clipboard";
+	import { ClipboardIconButton } from "@saas-ui/svelte/components/clipboard";
+
+	interface Props {
+		code: string;
+		html: string;
+		class?: string;
+	}
+
+	let { code, html, class: className }: Props = $props();
+</script>
+
+<Box class="code-block relative group {className ?? ''}">
+	<Box class="absolute right-2 top-2 opacity-0 group-hover:opacity-100 transition-opacity z-10">
+		<Clipboard.Root value={code}>
+			<ClipboardIconButton />
+		</Clipboard.Root>
+	</Box>
+	<Box class="shiki-wrapper rounded-lg overflow-x-auto text-sm">
+		{@html html}
+	</Box>
+</Box>
